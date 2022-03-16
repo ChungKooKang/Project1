0d4aa00af9aa0419f9f925769b78e8d8fae844f3
ChungKooKang <rkdcndrnwg@gmail.com>
2022-03-16 17:00:27 +0900

    시간복잡도와 공간복잡도 공부 위한 예시 코드

--- main.cpp
+++ main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <chrono>
+using Comparison = bool (*)(int, int);
+
+bool Ascending(int x, int y)
+{
+	return x < y;
+}
+bool Descending(int x, int y)
+{
+	return x > y;
+}
+void Sort(int numbers[], int count, Comparison f)
+{
+	for (int i = 0; i < count; i++)
+	{
+		for (int j = i + 1; j < count; j++)
+		{
+			if (f(numbers[i], numbers[j]))
+			{
+				int temp;
+				temp = numbers[i];
+				numbers[i] = numbers[j];
+				numbers[j] = temp;
+			}
+		}
+	}
+}
 int main()
 {
-	std::cout << "Hello wolrd!" << std::endl;
+	const int CountOfArray = 10;
+	int scores[CountOfArray]{ 20, 10, 40, 15, 30, 70, 60, 90, 80, 40 };
+	
+	// 현재시간
+	auto startTime = std::chrono::system_clock::now();
+	Sort(scores, CountOfArray, Ascending);
+	// 현재시간
+	auto endTime = std::chrono::system_clock::now();
+	for (int i = 0; i < CountOfArray; i++)
+	{
+		std::cout << scores[i] << std::endl;
+	}
+	auto duration = endTime - startTime;
+	std::cout << "Sort() runs " << duration.count() << "ms" << std::endl;
 	return 0;
 }
-
