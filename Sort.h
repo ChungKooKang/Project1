27cfa5c7129114840144ce65d5e68bfcfd485815
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-03-18 18:22:56 +0900

    Sorting 연습을 통한 좋은 코딩의 예시 연습하기

---
+++ Sort.h
@@ -0,0 +1,17 @@
+#pragma once
+
+void PrintArray(int input[], int size);
+
+void Swap(int& value1, int& value2);
+
+void SequentialSort(int input[], int size);
+
+void SelectionSort(int input[], int size);
+
+void BubbleSort(int input[], int size);
+
+void InsertionSort(int input[], int size);
+
+void Merge(int input[], int start, int half, int end, int temp[]);
+
+void MergeSort(int input[], int start, int end, int temp[]);
--- main.cpp
+++ main.cpp
@@ -1,46 +1,12 @@
-#include <iostream>
-#include <chrono>
-using Comparison = bool (*)(int, int);
-
-bool Ascending(int x, int y)
-{
-	return x < y;
-}
-bool Descending(int x, int y)
-{
-	return x > y;
-}
-void Sort(int numbers[], int count, Comparison f)
-{
-	for (int i = 0; i < count; i++)
-	{
-		for (int j = i + 1; j < count; j++)
-		{
-			if (f(numbers[i], numbers[j]))
-			{
-				int temp;
-				temp = numbers[i];
-				numbers[i] = numbers[j];
-				numbers[j] = temp;
-			}
-		}
-	}
-}
+# include <iostream>
+# include "Sort.h"
 int main()
 {
-	const int CountOfArray = 10;
-	int scores[CountOfArray]{ 20, 10, 40, 15, 30, 70, 60, 90, 80, 40 };
-	
-	// 현재시간
-	auto startTime = std::chrono::system_clock::now();
-	Sort(scores, CountOfArray, Ascending);
-	// 현재시간
-	auto endTime = std::chrono::system_clock::now();
-	for (int i = 0; i < CountOfArray; i++)
-	{
-		std::cout << scores[i] << std::endl;
-	}
-	auto duration = endTime - startTime;
-	std::cout << "Sort() runs " << duration.count() << "ms" << std::endl;
+	const int SIZE{ 5 };
+	int array[SIZE]{ 8, 7, 2, 3, 1 };
+	int tempArray[SIZE]{};
+
+	MergeSort(array, 0, SIZE - 1, tempArray);
+	PrintArray(array, SIZE);
 	return 0;
 }
