08570d7285a31d0241fb71fda9d880762337d8cb
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-03-28 16:10:28 +0900

    class 설명 예시인 고양이

--- main.cpp
+++ main.cpp
@@ -5,14 +5,21 @@
 # include "LinkedList.h"
 int main()
 {
-	MonsterList2 myList{};
-	CreateMonster(myList, "Wolf", 10);
-	CreateMonster(myList, "Slime", 20);
-	CreateMonster(myList, "Demon", 30);
-	std::cout << "<<숫자뽑기>>" << std::endl
-		<< "몬스터 숫자 : " << GetCountMonsterList(myList) << std::endl << std::endl;
-	std::cout << "<<몬스터 리스트 업>>" << std::endl; PrintMonsterList(myList);
-	std::cout << "<<몬스터 찾기>>" << std::endl; FindMonster(myList, "Wolf");
-	std::cout << "<<싹 다 지우기>>" << std::endl; DeleteAll()
+	class Cat // 변수와 함수를 묶음
+	{
+		//state
+		char name[20];
+		char breed[20];
+		int age;
+
+
+		// Behaviour
+		void Meow();
+		void Grooming();
+		void Sit();
+
+	}
+
+
 	return 0;
 }
