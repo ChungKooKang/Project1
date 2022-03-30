e029dfcfd41ec0aaf16497e6c963d6709457d85e
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-03-30 16:55:52 +0900

    class의 constructor destructor 연습

--- main.cpp
+++ main.cpp
@@ -5,14 +5,28 @@
 # include "LinkedList.h"
 # include "Cat.h"
 # include "Coordinate2D.h"
+class Myclass
+{
+private :
 
+public :
+
+	Myclass()  // Constructor
+	{
+		std::cout << "[Constructor]" << std::endl;
+	}
+
+	~Myclass()  // Destructor
+	{
+		std::cout << "[Destructor]" << std::endl;
+	}
+};
 
 int main()
 {
-	Coordinate2D Test;
-	Test.SetXY(1, 2);
-	std::cout << " X : " << Test.GetX() << " Y : " << Test.GetY() << std::endl;
-	std::cout << "원점에서의 거리 : " << Test.LengthFromZero() << std::endl;
-	Test.GetXY();
+	Myclass c;
+
+	std::cout << "c instance created!" << std::endl;
+
 	return 0;
 }
