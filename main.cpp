3164bbc5fa6bfa5e41bb0b58a2ac179fc0af2de2
rkdcndrnwg@gmail.com <rkdcndrnwg@gmail.com>
2022-04-04 17:02:42 +0900

    static, friend 수업 내용

--- main.cpp
+++ main.cpp
@@ -1,32 +1,38 @@
 # include <iostream>
-# include "Sort.h"
-# include "Literal.h"
-# include "DataStructure.h"
-# include "LinkedList.h"
-# include "Cat.h"
-# include "Coordinate2D.h"
-class Myclass
+
+class Sword
 {
-private :
-
-public :
-
-	Myclass()  // Constructor
+	friend class Warrior; // 이건 됨 왜 ? 이건 warrior가 나올 건데 그것을 가져오겠다 하는 것 이어서
+	friend void Warrior::AttackWith(Sword& word); // 이거 안됨 왜? 이것은 Warrior가 나왔는데 활용하겠다라는 뜻
 	{
-		std::cout << "[Constructor]" << std::endl;
+		sword.mDamage *= 2;
+		std::cout << "강화성공 :" << sword.mDamage << std::endl;
 	}
 
-	~Myclass()  // Destructor
+private :
+	int mDamage;
+public :
+	Sword(int dmg) : mDamage{ dmg }
 	{
-		std::cout << "[Destructor]" << std::endl;
 	}
 };
 
+class Warrior
+{
+public :
+	void AttackWith(Sword& sword)
+	{
+		std::cout << "칼을 휘둘러 " << sword.mDamage <<"만큼의 피해를 주었다!" << std::endl;
+	}
+	void Denfend();
+};
+
+
 int main()
 {
-	Myclass c;
-
-	std::cout << "c instance created!" << std::endl;
-
+	Sword shortSword(10);
+	Warrior player1;
+	UpgradeSword(shortSword);
+	player1.AttackWith(shortSword);
 	return 0;
 }
