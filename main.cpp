6fbccd63c15508da296c63e1c102eefa45ff17c0
rkdcndrnwg@gmail.com <rkdcndrnwg@gmail.com>
2022-04-06 17:03:25 +0900

    operator overloading practice end

--- main.cpp
+++ main.cpp
@@ -11,18 +11,44 @@ public :
 	{
 
 	}
-	operator const float()
+/*	operator const float()
 	{
 		return sqrt(mX * mX + mY * mY);
 	}
+*/
+	void operator () (int x, int y)
+	{
+		mX = x;
+		mY = y;
+	}
+	void operator () ()
+	{
+		mX = 0;
+		mY = 0;
+	}
+	void Print()
+	{
+		std::cout << "(" << mX << ", " << mY << ")" << std::endl;
+	}
+	friend std::ostream& operator << (std::ostream &os, const Point2D& pt);
 };
+std::ostream& operator << (std::ostream &os,const Point2D& pt)
+{
+	os << "(" << pt.mX << ", " << pt.mY << ")" << std::endl;
+	return os;
+}
+
 
 
 int main()
 {
-	Point2D pt1{ 2, 3 };
+	Point2D pt1{ 2, 3 }, pt2{3, 4};
+	// function + object == functor
+	pt1();
+	pt1.Print();
 
-	float distance = pt1;
+	pt1(3, 3);
+	pt1.Print();
 
-	std::cout << distance << std::endl;
+	std::cout << pt1 << pt2;
 }
