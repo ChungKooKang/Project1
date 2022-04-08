61e66de03d867cabc82257150c68dc5cf0033eb3
rkdcndrnwg@gmail.com <rkdcndrnwg@gmail.com>
2022-04-08 15:12:17 +0900

    is-part-of 예시

--- main.cpp
+++ main.cpp
@@ -1,37 +1,23 @@
 # include <iostream>
 
-class MyClass
+class CPU
 {
-public :
-	int mValue;
-
-	MyClass()
-	{
-		std::cout << "MyClass()" << std::endl;
-	}
-
-	MyClass(int x) : mValue{ x }
-	{
-		std::cout << "MyClass(int)" << std::endl;
-	}
-
-	MyClass(const MyClass& x) : mValue{ x.mValue } // 이건 자동으로 생성된다.
-	{
-		std::cout << "MyClass(MyClass&)" << std::endl;
-	}
-
-	MyClass& operator = (const MyClass& x)
-	{
-		std::cout << "operator = " << std::endl;
-		return *this;
-	}
 
 };
 
+class GraphicCard
+{
+
+};
+class Notebook
+{
+	CPU mCPU;		// 멤버변수화
+	GraphicCard mGPU;
+};
 int main()
 {
-	MyClass c1{ MyClass {1} }; // 이것도 만찬가지로 r-value 이기 때문에 복사가 안 일어나고 value에 이름만 붙여진다.
-	MyClass c2;  //이렇게 해야 복사 생성자가 일을 한다.
-	c2 = c1; // 대입연산자가 기본으로 이것을 허용한다. 이것을 copy assignment operator라고 한다.
+	Notebook gram, mackbook, omem;
+
+
 	return 0;
 }
