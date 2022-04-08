84d3a49b0d5027b75677aede20855f8bcae7d35f
rkdcndrnwg@gmail.com <rkdcndrnwg@gmail.com>
2022-04-08 15:34:47 +0900

    has-a 관계성 연습

--- main.cpp
+++ main.cpp
@@ -9,15 +9,23 @@ class GraphicCard
 {
 
 };
+class Keyboard
+{
+
+};
 class Notebook
 {
-	CPU mCPU;		// 멤버변수화
+	CPU mCPU;				// is-part-of : 멤버변수화
+	Keyboard* mKeyboard;	// has-a      : 멤버 포인터 변수화
 	GraphicCard mGPU;
 };
 int main()
 {
 	Notebook gram, mackbook, omem;
+	Keyboard logitech;
 
+	gram->Keyboard = logitech;
+	
 
 	return 0;
 }
