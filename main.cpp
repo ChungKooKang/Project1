cd55b23d87e9a9b1419f8652a18401297a1f9ab6
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-14 16:54:48 +0900

    template에 대한 연습

--- main.cpp
+++ main.cpp
@@ -1,37 +1,35 @@
 # include <iostream>
 
-class MyClass
+using namespace std;
+
+template <typename T>
+
+void Sort(T input[], int size)
 {
-public :
-	int mValue;
-
-	MyClass()
+	for (int i = 0; i < size - 1; ++i)
 	{
-		std::cout << "MyClass()" << std::endl;
+		for (int j = i + 1; j < size; ++j)
+		{
+			if (input[i] > input[j])
+			{
+				T temp = input[i];
+				input[i] = input[j];
+				input[j] = temp;
+			}
+		}
 	}
-
-	MyClass(int x) : mValue{ x }
-	{
-		std::cout << "MyClass(int)" << std::endl;
-	}
-
-	MyClass(const MyClass& x) : mValue{ x.mValue } // 이건 자동으로 생성된다.
-	{
-		std::cout << "MyClass(MyClass&)" << std::endl;
-	}
-
-	MyClass& operator = (const MyClass& x)
-	{
-		std::cout << "operator = " << std::endl;
-		return *this;
-	}
-
-};
+}
 
 int main()
 {
-	MyClass c1{ MyClass {1} }; // 이것도 만찬가지로 r-value 이기 때문에 복사가 안 일어나고 value에 이름만 붙여진다.
-	MyClass c2;  //이렇게 해야 복사 생성자가 일을 한다.
-	c2 = c1; // 대입연산자가 기본으로 이것을 허용한다. 이것을 copy assignment operator라고 한다.
+	int array[]{ 5, 3, 2, 4, 1 };
+
+	Sort<int>(array, 5);
+
+	for (int i = 0; i < 5; ++i)
+	{
+		cout << array[i] << endl;
+
+	}
 	return 0;
 }
