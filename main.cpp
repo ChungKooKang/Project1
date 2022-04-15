245433f98a4af74512336973a852b91ddd58a4ec
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-15 16:09:18 +0900

    여러가지 std string관련 연습

--- main.cpp
+++ main.cpp
@@ -1,73 +1,16 @@
 # include <iostream>
-// 게임 개발자
-class GameDeveloper
+# include <string>
+void Print(const std::string& str)
 {
-public :
-	std::string mName;
-	int mAge;
-
-using namespace std;
-
-};
-class Programmer : public GameDeveloper
-{
-
-public :
-	void Coding();
-	void Debugging();
-};
-
-class Designer : public GameDeveloper
-{
-public :
-	void Documentation();
-	void Presentation();
-};
-
-class ChiefProgrammer : public Programmer
-{
-public :
-	void Architecture();
-	void Scheduling();
-};
-class ChiefDesinger : public Designer
-{
-public :
-	void Meeting();
-	void Scheduling();
-};
-int main()
-{
-	Animal a;
-	a.mAge = 10; // (x)
-template <typename T>
-
-void Sort(T input[], int size)
-{
-	for (int i = 0; i < size - 1; ++i)
-	{
-		for (int j = i + 1; j < size; ++j)
-		{
-			if (input[i] > input[j])
-			{
-				T temp = input[i];
-				input[i] = input[j];
-				input[j] = temp;
-			}
-		}
-	}
+	std::cout << str.size() << " : " << str << std::endl;
 }
 
 int main()
 {
-	int array[]{ 5, 3, 2, 4, 1 };
+	int number = 123;
+	std::string str;
+	str = std::to_string(number);			// 123 -> "123"
+	number = std::stoi(str);				// "123" - > 123
 
-	Sort<int>(array, 5);
-
-	for (int i = 0; i < 5; ++i)
-	{
-		cout << array[i] << endl;
-
-	}
 	return 0;
 }
