be2f3f196b1657d90de8a8c11a0f99e86d114861
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-18 16:25:36 +0900

    STL qsort  Compare 한 줄로 만들기

--- main.cpp
+++ main.cpp
@@ -7,16 +7,9 @@ int Compare(const void* left, const void* right)
 	int arg1 = *static_cast<const int*>(left);
 	int arg2 = *static_cast<const int*>(right);
 
-	if (arg1 > arg2)
-	{
-		return 1;
-	}
-	else if (arg1 < arg2)
-	{
-		return -1;
-	}
+
 	
-	return 0;
+	return arg1 - arg2;
 }
 
 int main()
