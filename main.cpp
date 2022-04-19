1f69057694f2763a0a36b8f74318c1e2cc837c7a
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-19 16:25:53 +0900

    std::vector  연습

--- main.cpp
+++ main.cpp
@@ -9,23 +9,31 @@
 
 int main()
 {
-	std::array<int, 4> intArray{ 0, 1, 2, 3 };
+	std::vector<int> container {0, 1, 2, 3, 4};
+	//std::array<int, 4> container{ 0, 1, 2, 3 };
 
-	intArray.empty();  // array가 비었는지 아닌지 확인
-	intArray.fill(4);  // array를 4로 채움
+	//container.push_back(3);  // 뒤에 원소가 하나 추가되며 이것은 O(1)이다.
+	//container.pop_back();	 // 뒤에 원소 하나를 뺴 버리며 이것 또한 복잡도가 O(1)이다.
+	//container.erase(container.begin()); // 특정 원소만 지우기 기거는 overhead가 일어나 느리게 된다.
+	//container.resize(10, 100);				// 배열의 크기를 조정하고 남는 공간에 '100'를 넣는다.
+	container.clear();					//원소들이 싹 없어진다.
 
-	for (const auto& e : intArray)
+
+	for (const auto& e : container)
 	{
-		std::cout << e << std::endl;
+		std::cout << e << " ";
 	}
+	std::cout << std::endl;
 
-	for (auto itr = intArray.begin(); itr != intArray.end(); itr++)
+	for (auto itr = container.begin(); itr != container.end(); itr++)
 	{
-		std::cout << *itr << std::endl;
+		std::cout << *itr << " ";
 	}
+	std::cout << std::endl;
 
-	for (int i = 0; i < intArray.size(); ++i)
+	for (int i = 0; i < container.size(); ++i)
 	{
-		std::cout << intArray[i] << std::endl;
+		std::cout << container[i] << " ";
 	}
+	std::cout << std::endl;
 }
