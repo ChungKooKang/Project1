2833b0d83d8268304728062d3061a1f61c28c38d
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-19 15:38:45 +0900

    std::array  연습

--- main.cpp
+++ main.cpp
@@ -3,41 +3,27 @@
 # include <vector>
 # include <algorithm>
 # include <numeric>
-int Compare(const void* left, const void* right)
-{
-	int arg1 = *static_cast<const int*>(left);
-	int arg2 = *static_cast<const int*>(right);
-	return arg1 - arg2;
-}
+# include <array>
 
 
 
 int main()
 {
-	
-	std::vector<int> v{ 3, 1, 3, 2, 5 };
-	int v2[]{ 3, 4, 1, 5, 2 };
-
-	// qsort 연습 & lambda exprssion 연습
-	std::qsort(&v[0], v.size(), sizeof(int), [](const void* left, const void* right)->int {
-		int arg1 = *static_cast<const int*>(left);
-		int arg2 = *static_cast<const int*>(right);
-		return arg1 - arg2;
-		});
-
-	// qsort 연습 C 버전
-	std::qsort(&v2[0], 5, sizeof(int), Compare);
-
-	// sort  연습
-	std::sort(v.begin(), v.end());
-
-	//acumulate 연습
-	std::cout << "합계 : " << std::accumulate(v.begin(), v.end(), 0) << std::endl;
+	std::array<int, 4> intArray{ 0, 1, 2, 3 };
 
 
-	for (const auto& e : v)			// 빠르게 불러올 수 있다. 왜냐하면 copy가 일어나지 않기 때문이다.
+	for (const auto& e : intArray)
 	{
 		std::cout << e << std::endl;
 	}
 
+	for (auto itr = intArray.begin(); itr != intArray.end(); itr++)
+	{
+		std::cout << *itr << std::endl;
+	}
+
+	for (int i = 0; i < intArray.size(); ++i)
+	{
+		std::cout << intArray[i] << std::endl;
+	}
 }
