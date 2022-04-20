05942fcca36e1cb0582fc9b5ba1d64ebee7dc02a
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-20 16:54:35 +0900

    std::pair와 std::map 연습

--- main.cpp
+++ main.cpp
@@ -11,15 +11,40 @@
 # include <queue>
 # include <set>
 # include <map>
+
+using MyPair = std::pair<std::string, std::string>;
 int main()
 {
-	std::set<int> container{ 8, 3, 1 , 6, 10, 27, 4, 5 };
+	std::map<std::string, std::string> container;
+	// "array" : " 단순 배열"
+	// "vector" : " 동적배열"
+	// " stack " : "LIOF 구조"
+	std::pair < std::string, std::string> element;
+	element.first = "array";
+	element.second = "Simple Static array";
+	container.insert(element);
+
+	// constructor를 활용해서 만들기
+	container.insert(std::pair<std::string, std::string>("vector", "Dynamic array"));
+	
+	// using alias
+	container.insert(MyPair("stack", "LIFO"));
+
+	// uniform initialization
+	container.insert({ "queue", "FIFO" });
 
 	for (const auto& e : container)
 	{
-		std::cout << e << " ";
+		std::cout << e.first << " : " << e.second;
+		std::cout  << std::endl;
 	}
-	
 
+	std::cout << container.at("queue") << std::endl;
+
+	//==
+
+	std::cout << container["queue"] << std::endl;
+	
+	
 	std::cout << std::endl;
 }
