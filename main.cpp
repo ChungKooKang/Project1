a68eaa1ff182a42ad81f9eee9e157961a62f5166
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-20 16:30:52 +0900

    set 연

--- main.cpp
+++ main.cpp
@@ -9,22 +9,17 @@
 # include <deque>
 # include <stack>
 # include <queue>
-
+# include <set>
+# include <map>
 int main()
 {
-	std::priority_queue<int> container;
+	std::set<int> container{ 8, 3, 1 , 6, 10, 27, 4, 5 };
+
+	for (const auto& e : container)
+	{
+		std::cout << e << " ";
+	}
 	
-	container.push(1);
-	container.push(1);
-	container.push(1);
-	container.push(2);
-	container.push(2);
-	container.push(3);
-	container.push(3);
-	container.push(1);
-
-	std::cout << container.top() << std::endl; // queue인데 front가 아니라 top를 사용하는 이유는 제일 먼저 실행하는 우선순위가 있기 때문이다. 이때 우선순위는 내림차순으로 된다.
-
 
 	std::cout << std::endl;
 }
