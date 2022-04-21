5df1c0a783ef17e7f7aae4b735b7ded6b46f644e
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-21 16:01:46 +0900

    학생들 관리 문제 내 답

--- main.cpp
+++ main.cpp
@@ -12,39 +12,121 @@
 # include <set>
 # include <map>
 
-using MyPair = std::pair<std::string, std::string>;
+class Student
+{
+private :
+
+
+public :
+	int mNumber;
+	std::string mName;
+	int mScore;
+
+	void Print()
+	{
+		std::cout << "(" << mNumber << ")" <<
+			mName << " : " << mScore << std::endl;
+	}
+};
+
+using Students = std::vector<Student>;
+void AddStudent(Students& v)
+{
+	// 번호, 이름, 점수 입력
+	Student temp;
+	std::cout << "번호를 입력하세요 : ";
+	std::cin >> temp.mNumber;
+	std::cout << std::endl;
+
+	std::cout << "이름을 입력하세요 : ";
+	std::cin >> temp.mName;
+	std::cout << std::endl;
+
+	std::cout << "점수를 입력하세요 : ";
+	std::cin >> temp.mScore;
+	std::cout << std::endl;
+	
+	v.push_back(temp);
+}
+
+void RemoveStudent(Students& v)
+{
+	// 번호로 삭제
+	int inputNumber;
+	std::vector<Student>::iterator itr;
+	std::cout << "삭제할 번호를 입력하세요 : ";
+	std::cin >> inputNumber;
+	std::find(v.begin(),v.end(), inputNumber);
+	v.erase()
+}
+
+void PrintStudents(const Students& v)
+{
+	for (const auto& e : v)
+	{
+		std::cout << e.mNumber << ". " << e.mName << " : " << e.mScore << std::endl;
+	}
+}
+
+void PrintScoreInfo(const Students& v)
+{
+
+}
+
+void PrintOverAverage(const Students& v)
+{
+
+}
+
 int main()
 {
-	std::map<std::string, std::string> container;
-	// "array" : " 단순 배열"
-	// "vector" : " 동적배열"
-	// " stack " : "LIOF 구조"
-	std::pair < std::string, std::string> element;
-	element.first = "array";
-	element.second = "Simple Static array";
-	container.insert(element);
+	// 1. Kim 80
+	// 2. Lee 20
+	// 3. Park 50
+	// 4. Choi 30
 
-	// constructor를 활용해서 만들기
-	container.insert(std::pair<std::string, std::string>("vector", "Dynamic array"));
+	std::vector<Student> students{
+		{1, "Kim", 80},
+		{2, "Lee", 20},
+		{3, "Park", 50},
+		{4, "Choi", 30}
+	};
 	
-	// using alias
-	container.insert(MyPair("stack", "LIFO"));
-
-	// uniform initialization
-	container.insert({ "queue", "FIFO" });
-
-	for (const auto& e : container)
+	int command{};
+	while (command != 6)
 	{
-		std::cout << e.first << " : " << e.second;
-		std::cout  << std::endl;
+		std::cout << "1. 학생 추가" << std::endl;
+		std::cout << "2. 학생 제거" << std::endl;
+		std::cout << "3. 전체 학생 출력" << std::endl;
+		std::cout << "4. 클래스 평균 및 총점" << std::endl;
+		std::cout << "5. 클래스 평규 이상의 학생 목록" << std::endl;
+		std::cout << "6. 종료" << std::endl;
+		
+		std::cout << "> ";
+		std::cin >> command;
+		switch (command)
+		{
+		case 1 :
+			AddStudent(students);
+			break;
+		case 2 :
+			RemoveStudent(students);
+			break;
+		case 3 :
+			PrintStudents(students);
+			break;
+		case 4 :
+			PrintScoreInfo(students);
+			break;
+		case 5 :
+			PrintOverAverage(students);
+			break;
+		case 6 :
+			break;
+		default :
+			std::cout << "잘못된 명령어 입니다." << std::endl;
+			break;
+
+		}
 	}
-
-	std::cout << container.at("queue") << std::endl;
-
-	//==
-
-	std::cout << container["queue"] << std::endl;
-	
-	
-	std::cout << std::endl;
 }
