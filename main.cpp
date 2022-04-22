fb9ebea2ae6a4acdca23483973bd763acb7249eb
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-22 16:52:15 +0900

    Raw string literal 연

--- main.cpp
+++ main.cpp
@@ -1,18 +1,8 @@
 # include <iostream>
 
-enum class MyEnum1
-{
-	Color = 1,
-	Alpha = 2
-};
-
-enum class MyEnum2
-{
-	Color = 1
-};
 
 int main()
 {
-	MyEnum1::Color;
-	MyEnum2::Color;
+	std::string str = R"("'\'")";
+	std::cout << str << std::endl;
 }
