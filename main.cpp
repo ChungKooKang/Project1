612609a555fdda9d61f93ece40a66c2e34b5084c
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-04-25 15:23:14 +0900

    Factory Pattern

--- main.cpp
+++ main.cpp
@@ -1,22 +1,14 @@
 # include <iostream>
 
-template<typename T,typename... Types>
-
-void Function()
+template<typename T, typename... Types>
+// Factory Pattern
+T* Make(Types... args)
 {
-	std::cout << "---end---" << std::endl;
-}
-
-void Function(T arg, Types... args)
-{
-	std::cout << arg << " ";
-
-	Function(args...);
+	return new T(args...);
 }
 
 
 int main()
 {
-	Function(1, 2, 3);
-	// Function(1, 1.0, "abc");
+
 }
