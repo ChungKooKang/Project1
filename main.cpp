81b899242fafb268f156d95ca47bf3c39e0b1978
ChungKoo Kang <rkdcndrnwg@gmail.com>
2022-05-20 20:08:47 +0900

    d2d1 연

--- main.cpp
+++ main.cpp
@@ -1,23 +1,121 @@
-# include <iostream>
+#include <Windows.h>
+#include <d2d1.h>
 
-class Image
+#pragma comment (lib, "d2d1.lib")
+
+
+const wchar_t gClassName[] = L"MyWindowClass";
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
+
+ID2D1Factory* gpD2DFactory{};
+ID2D1HwndRenderTarget* gpRenderTarget{};
+
+int WINAPI WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevIstance,
+	_In_ LPSTR lpCmdLine,
+	_In_ int nShowCmd
+)
 {
-	// 그림 파일
+	HRESULT hr = D2D1CreateFactory(
+		D2D1_FACTORY_TYPE_SINGLE_THREADED,
+		&gpD2DFactory
+	);
 
-};
-
-class Bug
-{
-	std::shared_ptr<Image> mspImage;
-
-public :
-	Bug(std::shared_ptr<Image> image) : mspImage{image}
+	if (FAILED(hr))
 	{
+		MessageBox(NULL, L"Failed To Create D2D Factory!", L"Error", MB_OK);
 
+		return 0;
 	}
-};
 
-int main()
+	WNDCLASSEX wc;
+
+	ZeroMemory(&wc, sizeof(WNDCLASSEX));
+	wc.style = CS_HREDRAW | CS_VREDRAW;
+	wc.lpszClassName = gClassName;
+	wc.hInstance = hInstance;
+	wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wc.hbrBackground = (HBRUSH)COLOR_WINDOW;
+	wc.lpfnWndProc = WindowProc;
+	wc.cbSize = sizeof(WNDCLASSEX);
+
+	if (!RegisterClassEx(&wc))
+	{
+		MessageBox(
+			nullptr,
+			L"Failed to register window Class!", L"Error", MB_ICONEXCLAMATION | MB_OK
+		);
+		return 0;
+	}
+
+	RECT rc{ 0, 0, 1024, 768 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+
+	HWND hwnd = CreateWindowEx(
+		NULL,
+		gClassName,
+		L"Direct2D",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		rc.right - rc.left,
+		rc.bottom - rc.top,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+		);
+
+	if (hwnd == nullptr)
+	{
+		MessageBox(nullptr, L"Failed to create window class!", L"Error", MB_ICONEXCLAMATION | MB_OK);
+		return 0;
+	}
+
+	ShowWindow(hwnd, nShowCmd);
+	UpdateWindow(hwnd);
+
+	MSG msg;
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return static_cast<int>(msg, msg.wParam);
+
+}
+
+void OnPaint(HWND hwnd)
 {
+	HDC hdc;
+	PAINTSTRUCT ps;
 
+	hdc = BeginPaint(hwnd, &ps);
+
+	EndPaint(hwnd, &ps);
+}
+
+LRESULT WindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_PAINT :
+		OnPaint(hwnd);
+		break;
+
+	case WM_CLOSE :
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY :
+		PostQuitMessage(0);
+		break;
+
+	default :
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+	return 0;
 }
